0bd59a2958aec46343b8fc0388ab3d798aa283a4
Gabriel Angelo Gerardino <gabriel_angelo_gerardino@dlsu.edu.ph>
2023-10-15 17:46:02 +0800

    added input fields. no constraints yet

--- app/src/main/java/com/example/planify/RegisterActivity.kt
+++ app/src/main/java/com/example/planify/RegisterActivity.kt
@@ -1,13 +1,13 @@
 package com.example.planify
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
+
+
     }
 }
