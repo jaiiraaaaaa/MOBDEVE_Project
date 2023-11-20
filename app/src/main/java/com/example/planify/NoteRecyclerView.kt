7aa53f8f3e02caebd9cb3dcbbad3322aaeec9edf
jaiiraaaaaa <94473609+jaiiraaaaaa@users.noreply.github.com>
2023-11-20 15:02:05 +0800

    notes recyclerview faulty

---
+++ app/src/main/java/com/example/planify/NoteRecyclerView.kt
@@ -0,0 +1,26 @@
+package com.example.planify
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+class HorizontalRecyclerView(private val notesList: List<NoteModel>) : RecyclerView.Adapter<HorizontalRecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_notes_card, parent, false)
+        return ViewHolder(view)
+    }
+    override fun getItemCount(): Int {
+        return notesList.size
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val note = notesList[position]
+        val titleTextView = holder.itemView.findViewById<TextView>(R.id.note_title_tv)
+
+        titleTextView.text = note.title
+    }
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleTextView: TextView = view.findViewById(R.id.note_title_tv)
+    }
+}
--- app/src/main/java/com/example/planify/TasksNotesActivity.kt
+++ app/src/main/java/com/example/planify/TasksNotesActivity.kt
@@ -2,12 +2,40 @@ package com.example.planify
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.planify.databinding.ActivityTasksNoteBinding
 
+class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(note: NoteModel) {
+        val titleTextView = itemView.findViewById<TextView>(R.id.note_name)
+        titleTextView.text = note.title
+    }
+}
+class NoteAdapter(private val notes: List<NoteModel>) : RecyclerView.Adapter<NoteViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_notes_row, parent, false)
+        return NoteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.bind(note)
+    }
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+}
 class TasksNotesActivity : AppCompatActivity() {
 
+    private lateinit var notesRecyclerView: RecyclerView
+    private lateinit var notesAdapter: HorizontalRecyclerView
+    private var notes = mutableListOf<NoteModel>()
     private lateinit var binding: ActivityTasksNoteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,5 +71,18 @@ class TasksNotesActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        notes = generateSampleNotes().toMutableList()
+        notesRecyclerView = findViewById(R.id.recycleNotes)
+        notesAdapter = HorizontalRecyclerView(notes)
+        notesRecyclerView.adapter = notesAdapter
+        notesRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+    }
+    private fun generateSampleNotes(): List<NoteModel> {
+        return listOf(
+            NoteModel(1, "Note 1", "Description", "11/20/23" ),
+            NoteModel(2,"Note 2", "Description", "11/21/23"),
+        )
     }
 }
